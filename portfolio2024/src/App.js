4b7900f9bd28dbd854a4b154e93db3900862dbfe
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2024-11-06 19:08:21 -0500

    Update App.js

--- portfolio2024/src/App.js
+++ portfolio2024/src/App.js
@@ -14,8 +14,7 @@ function App() {
     <Routes> 
       <Route path="/" element={<Home />} /> 
       <Route path="/about" element={<About />} /> 
-      <Route path="/projects" element={<Projects />} /> 
-      <Route path="/skills" element={<Skills />} /> 
+      <Route path="/projects" element={<Projects />} />  
       <Route path="/contact" element={<Contact />} /> 
       </Routes> 
       </Router>
